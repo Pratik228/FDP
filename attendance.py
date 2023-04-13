0f5958aa64e544d29b465a15f37febc8f230b2e4
Pratik228 <pratikmishra79@gmail.com>
2023-04-13 12:20:38 +0530

    few backend changes

--- attendance.py
+++ attendance.py
@@ -1,74 +1,3 @@
-# import streamlit as st
-# import cv2
-# import os
-# def main():
-#     st.title("Student Attendance System")
-#     menu = ["Store Student Details","Store Student Image", "Take Attendance", "Check Attendance"]
-#     choice = st.sidebar.selectbox("Select Option", menu)
-
-#     if choice == "Store Student Details":
-#         store_student_details()
-
-#     elif choice == "Store Student Image":
-#         store_image()
-
-#     elif choice == "Take Attendance":
-#         take_attendance()
-
-#     elif choice == "Check Attendance":
-#         check_attendance()
-    
-
-# def store_student_details():
-#     st.subheader("Store Student Details")
-#     # Add code to store student details and image
-
-# def store_image():
-#     st.subheader("Store Image")
-#     # Get the USN of the student from the user
-#     usn = st.text_input("Enter the USN of the student:")
-#     # Create the "photos" folder if it does not exist
-#     if not os.path.exists("Images"):
-#         os.makedirs("Images")
-#     # Initialize the webcam
-#     cap = cv2.VideoCapture(0)
-
-#     # Take a single photo
-#     while True:
-#         # Capture a frame from the webcam
-#         ret, frame = cap.read()
-
-#         # Display the frame
-#         cv2.imshow("Webcam", frame)
-
-#         # Check if the user pressed 'q' to quit
-#         if cv2.waitKey(1) & 0xFF == ord('q'):
-#             break
-
-#         # Check if the user clicked the left mouse button
-#         if cv2.waitKey(1) & 0xFF == ord(' '):  # Space bar is ASCII 32
-#             # Save the photo as "USN.jpg" in the "photos" folder
-#             file_name = f"Images/{usn}.jpg"
-#             cv2.imwrite(file_name, frame)
-#             st.write(f"Saved photo as {file_name}")
-#             break
-
-#     # Release the webcam
-#     cap.release()
-
-#     # Destroy all windows
-#     cv2.destroyAllWindows()
-
-# def take_attendance():
-#     st.subheader("Take Attendance")
-#     # Add code to take attendance using saved images
-
-# def check_attendance():
-#     st.subheader("Check Attendance")
-#     # Add code to check attendance for a particular student
-
-# if __name__ == '__main__':
-#     main()
 import streamlit as st
 import cv2
 import os
@@ -180,44 +109,9 @@ def store_image():
         cv2.destroyAllWindows()
 def store_encodings():
     st.subheader("Store Encodings")
-
-    # cred = credentials.Certificate("serviceAccountKey.json")
-    # firebase_admin.initialize_app(cred, {
-    #     'databaseURL': 'https://facialattendance-d2c63-default-rtdb.firebaseio.com/',
-    #     'storageBucket': 'facialattendance-d2c63.appspot.com'
-    # })
-    # ref = db.reference('Encodings')
-
-    # Load the encodings from the "EncodeFile.p" pickle file
-    with open("EncodeFile.p", "rb") as f:
-        encodeListKnown, studId = pickle.load(f)
-
-    # Iterate over the images in the "Images" folder and encode them
-    for image_file in os.listdir("Images"):
-        image_path = os.path.join("Images", image_file)
-        image_name = os.path.splitext(image_file)[0]
-
-        # Load the image
-        img = cv2.imread(image_path)
-
-        # Convert the image from BGR to RGB
-        rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-        # Find the face locations in the image
-        face_locations = face_recognition.face_locations(rgb_img)
-
-        # Encode the face(s) in the image
-        encodings = face_recognition.face_encodings(rgb_img, face_locations)
-
-        if len(encodings) > 0:
-            # Store the encodings in the Firebase Realtime Database
-            data = {
-                'encodings': encodings[0].tolist(),
-                'student_id': image_name
-            }
-            encodings_ref.push(data)
-
-    st.success('Success! Encodings stored.')
+    if st.button("Click to store encodings"):
+        subprocess.run(["python", "encoding.py"])
+        st.success('Success! Encodings stored.')  
 
 
 def take_attendance():
